91f195a22196db177912d9a2564f32161db472d2
Ludmele <ludmelefigueiredo@gmail.com>
2025-04-10 11:38:21 -0400

    Operadores

---
+++ correção.js
@@ -0,0 +1,32 @@
+// Correção do dia 10/04/2025
+
+// Exercício 1
+
+let nome = "Ludmele";
+
+let idade = 15;
+
+console.log("Meu nome é "+nome+ " e eu tenho "+ idade+ " anos");
+
+//Exercício 2
+
+let dog = "Pug";
+
+console.log(dog.toUpperCase());
+console.log(dog.toLowerCase());
+
+// Exercício 3
+
+let anos_de_carreira = 20;
+console.log(typeof anos_de_carreira);
+
+// Exercício 4
+
+let altura = 1.75;
+
+let peso = 70;
+
+let divisao_imc = (peso/(altura*altura));
+
+console.log(`Seu IMC é: ${divisao_imc.toFixed(2)}`);
+
