b76939008a29a6697bbdb705829c8794ac3fb66e
Ludmele Figueiredo da Silva <ludmelefigueiredo@gmail.com>
2025-04-10 11:42:17 -0400

    Add files via upload

---
+++ Operadores/exercício_02.js
@@ -0,0 +1,9 @@
+//2. Declare duas idades e utilize operadores de comparação para verificar se uma
+//pessoa é mais velha que a outra
+
+let idade1 = 16;
+let idade2 = 26;
+
+console.log(idade1 > idade2);
+console.log(idade1 < idade2);
+console.log(idade1 == idade2);
---
+++ Operadores/exercício_04.js
@@ -0,0 +1,8 @@
+//4. Crie uma variável com um número e use o operador ternário para verificar se é
+//par ou ímpar
+
+let num = 25;
+
+let verifique = num%2 == 0 ? "Par" : "Ímpar";
+
+console.log(verifique)
